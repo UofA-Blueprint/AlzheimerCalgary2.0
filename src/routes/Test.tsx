37ceffe6d39756c2fdecc0b91bb4bb136a85e1a0
Ho Tien Duc <hotienduc05012002@gmail.com>
2024-01-22 21:22:51 -0700

    Added Proxima Nova fonts and color themes

--- src/routes/Test.tsx
+++ src/routes/Test.tsx
@@ -1,5 +1,10 @@
 function Test() {
-  return <div>Test</div>;
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-status-red-main text-white">
+      <div className="font-display text-6xl">Display 1</div>
+      <div className="font-display text-6xl font-bold">Display 1</div>
+    </div>
+  );
 }
 
 export default Test;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build'
+    outDir: "build",
   },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
