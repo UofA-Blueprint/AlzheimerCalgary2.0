3a6d83b26186e80174c0ff3ec4dd1c66127f6c9f
woopy098 <randy.luna98@ymail.com>
2024-02-20 01:53:28 -0700

    code cleanup

--- src/routes/ProfilePictureTestRoute.tsx
+++ src/routes/ProfilePictureTestRoute.tsx
@@ -1,12 +1,21 @@
 import ProfilePictures from "@/components/ProfilePictures";
-import profilePic from "@/assets/images/face1.jpeg";
+import profilePic from "@/assets/images/face2.jpg";
+import profilePic1 from "@/assets/images/face1.jpeg";
+import profilePic2 from "@/assets/images/face.jpeg";
+
+import * as Icon from '@phosphor-icons/react'
 
 function ProfilePictureTest() {
+
+  
   return (
-    <div>
-      {/* Using margin or padding for the parent tag can break the navigation bar. */}
-      <ProfilePictures children={profilePic} backgroundColor="gold"/>
-    </div>
+    <div className="space-y-4 p-4">
+      <ProfilePictures backgroundColor="tulip"><img className="w-full h-full object-cover rounded-full" src={profilePic} alt="Profile pic" /></ProfilePictures>
+      <ProfilePictures backgroundColor="tulip"><img className="w-full h-full object-cover rounded-full" src={profilePic1} alt="Profile pic" /></ProfilePictures>
+      <ProfilePictures backgroundColor="tulip"><img className="w-full h-full object-cover rounded-full" src={profilePic2} alt="Profile pic" /></ProfilePictures>
+
+      <ProfilePictures backgroundColor="tulip"><Icon.PawPrint className="w-full h-full object-cover rounded-full" size={100} color={'white'}/></ProfilePictures>    
+      </div>
   );
 }
 
