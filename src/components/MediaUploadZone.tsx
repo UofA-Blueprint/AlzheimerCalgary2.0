88426ea0d4cb951c7c8197dc061ef77c652dd67c
khanheric <duykhanh.eric@gmail.com>
2024-02-26 02:40:32 -0700

    adjusted

--- src/components/MediaUploadZone.tsx
+++ src/components/MediaUploadZone.tsx
@@ -3,42 +3,30 @@ import { useState, useEffect, useRef } from "react";
 
 interface MediaUploadZoneProps {
   onFilesDropped: (files: File[]) => void;
+  fontSize?: string;
+  className?: string;
 }
 
 const MediaUploadZone: React.FC<MediaUploadZoneProps> = ({
   onFilesDropped,
+  fontSize = "1em",
 }) => {
   // parent ref
   const parentRef = useRef<HTMLDivElement>(null);
 
   // handling sizing
-  const [margin, setMargin] = useState("1em");
-  const [fontSize, setFontSize] = useState("1em");
   // handling drop in file
   const [isDragOver, setIsDragOver] = useState(false);
   const [droppedFiles, setDroppedFiles] = useState<File[]>([]);
   const [isValidFile, setIsValidFile] = useState(true);
 
   const UploadZoneClassName =
-    "w-full h-full border-2 border-dashed flex flex-col justify-center items-center rounded-md border-primary-main " +
+    "w-full h-full border-2 border-dashed flex flex-col justify-center items-center rounded-md border-primary-main text-base" +
     (isDragOver
       ? " transform transition-transform duration-300 border-primary-dark ease-in-out scale-95"
       : "");
 
-  // adjust font size according to parent ref
-  useEffect(() => {
-    if (parentRef.current) {
-      const { width, height } = parentRef.current.getBoundingClientRect();
-      const newSize = Math.min(width, height) / 15;
-      setFontSize(`${newSize}px`);
-      setMargin(`${newSize / 10}px  `);
-      width > 700
-        ? setMargin(`${newSize / 10}px`)
-        : setMargin(`${-newSize / 2}px`);
-      console.log(width);
-    }
-  }, []);
-
+  // handle drop events, drop the PNG, JPEG and GIF type of file
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     const files = Array.from(event.dataTransfer.files);
@@ -77,64 +65,59 @@ const MediaUploadZone: React.FC<MediaUploadZoneProps> = ({
   }, [droppedFiles]);
 
   return (
-    <div className="w-full h-full" style={{ fontSize }} ref={parentRef}>
+    <div
+      className="w-full h-full "
+      ref={parentRef}
+      style={{ fontSize: fontSize }}
+    >
       <div
         className={UploadZoneClassName}
         onDrop={handleDrop}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
+        style={{ fontSize: fontSize }}
       >
-        <UploadSimple className="mb-2" style={{ transform: "scale(2)" }} />
-
-        <div style={{ margin }} className="">
-          Drag and Drop here
-        </div>
-        <div style={{ margin }}> or </div>
-        <input
-          type="file"
-          id="fileInput"
-          style={{ display: "none" }}
-          onChange={(event) => {
-            if (event.target.files) {
-              const files = Array.from(event.target.files);
-              const validFiles = filterValidFiles(files);
-              setDroppedFiles((prevFiles) => [...prevFiles, ...validFiles]);
-            }
-          }}
-        />
-        <label
-          style={{ margin }}
-          htmlFor="fileInput"
-          className="text-primary-main cursor-pointer"
+        <div
+          className="flex flex-col  justify-center items-center text-base	shrink"
+          style={{ fontSize: fontSize }}
         >
-          Browse file
-        </label>
-        {droppedFiles.length > 0 && (
-          <div
-            className="truncate w-5/6 flex flex-col items-center overflow-hidden"
-            style={{ margin }}
+          <UploadSimple className="mb-2" style={{ transform: "scale(2)" }} />
+          <div className="">Drag and Drop hered</div>
+          <div className=""> or </div>
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={(event) => {
+              if (event.target.files) {
+                const files = Array.from(event.target.files);
+                const validFiles = filterValidFiles(files);
+                setDroppedFiles((prevFiles) => [...prevFiles, ...validFiles]);
+              }
+            }}
+          />
+          <label
+            htmlFor="fileInput"
+            className="text-primary-main cursor-pointer mb-3"
           >
+            Browse file
+          </label>
+        </div>
+        {droppedFiles.length > 0 && (
+          <div className="truncate w-5/6 flex flex-col items-center overflow-hidden">
             {droppedFiles.slice(0, 3).map((file, index) => (
-              <div
-                style={{ margin }}
-                key={index}
-                className="truncate w-full text-center"
-              >
+              <div key={index} className="truncate w-full text-center">
                 {file.name}
               </div>
             ))}
-            {droppedFiles.length > 3 && <div style={{ margin }}>...</div>}
+            {droppedFiles.length > 3 && <div>...</div>}
           </div>
         )}
       </div>
-      <div
-        className="flex flex-row justify-between text-xs text-gray-500 mt-0.5"
-        style={{ fontSize }}
-      >
+      <div className="flex flex-row justify-between text-xs text-gray-500 mt-0.5">
         <div>Accepted file types: png, jpeg</div>
         <div>Max size: 25MB</div>
       </div>
-
       {!isValidFile && (
         <div className="flex flex-row text-xs  text-red-500">
           <WarningCircle className="mt-0.5 mr-0.5"></WarningCircle> File type
