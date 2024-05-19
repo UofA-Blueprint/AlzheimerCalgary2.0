89e768e42938a9390a8634da3fe35a8073f0a013
Thea Nguyen <thea25060@gmail.com>
2024-05-18 23:39:32 -0600

    Created MemberTable.tsx + MemberTableTestRoute.tsx

--- src/routes/ProfilePictureTestRoute.tsx
+++ src/routes/ProfilePictureTestRoute.tsx
@@ -1,52 +1,28 @@
 import ProfilePictures from "@/components/ProfilePictures";
 import profilePic from "@/assets/images/face2.jpg";
-import profilePic1 from "@/assets/images/face1.jpeg";
-import profilePic2 from "@/assets/images/face.jpeg";
 
 import * as Icon from "@phosphor-icons/react";
 
 function ProfilePictureTest() {
-  return (
-    <div className="space-y-4 p-4">
-      <div className="w-12 h-12">
-        <ProfilePictures backgroundColor="tulip">
-          <img
-            className="w-full h-full object-cover rounded-full"
-            src={profilePic}
-            alt="Profile pic"
-          />
-        </ProfilePictures>
-      </div>
+	return (
+		<div className="space-y-4 p-4">
+			<div className="w-20 h-20">
+				<ProfilePictures backgroundColor="tulip">
+					<img
+						src={profilePic}
+						alt="profile"
+						className="w-12 h-12 rounded-full object-cover"
+					/>
+				</ProfilePictures>
+			</div>
 
-      <div className="w-20 h-20">
-        <ProfilePictures backgroundColor="tulip">
-          <img
-            className="w-full h-full object-cover rounded-full"
-            src={profilePic1}
-            alt="Profile pic"
-          />
-        </ProfilePictures>
-      </div>
-      <div className="w-24 h-24">
-        <ProfilePictures backgroundColor="tulip">
-          <img
-            className="w-full h-full object-cover rounded-full"
-            src={profilePic2}
-            alt="Profile pic"
-          />
-        </ProfilePictures>
-      </div>
-      <div className="w-12 h-12">
-        <ProfilePictures backgroundColor="tulip">
-          <Icon.PawPrint
-            className="w-full h-full object-cover rounded-full"
-            size={100}
-            color={"white"}
-          />
-        </ProfilePictures>
-      </div>
-    </div>
-  );
+			<div className="w-12 h-12">
+				<ProfilePictures backgroundColor="tulip">
+					<Icon.PawPrint className="w-full h-full rounded-full p-2 object-cover" />
+				</ProfilePictures>
+			</div>
+		</div>
+	);
 }
 
 export default ProfilePictureTest;
