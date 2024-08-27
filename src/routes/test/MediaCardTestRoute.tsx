56251dd2b64c947ca4a88640cc4cbfc7fe0c82cb
Duc Ho <hotienduc05012002@gmail.com>
2024-08-26 22:52:18 -0600

    Added to route more mock data

--- src/routes/test/MediaCardTestRoute.tsx
+++ src/routes/test/MediaCardTestRoute.tsx
@@ -2,10 +2,40 @@ import MediaCard from "@/components/MediaCard";
 
 function MediaCardTestRoute() {
 	return (
-		<div className="bg-blue-200 h-screen flex justify-center items-center">
-			<div className="w-[360px]">
+		<div className="bg-blue-200 min-h-screen flex justify-center items-center p-4">
+			<div className="grid grid-cols-3 gap-4">
 				<MediaCard
 					src="https://images.unsplash.com/photo-1724579243894-6a8c9bbfe88c"
+					title="Channeling their inner artist with a paintbrush and a splash of color!"
+					id={1234}
+					date={new Date("Oct 2, 2024")}
+				/>
+				<MediaCard
+					src="https://images.unsplash.com/photo-1723984834599-5357b87f727c"
+					title="Channeling their inner artist with a paintbrush and a splash of color!"
+					id={1234}
+					date={new Date("Oct 2, 2024")}
+				/>
+				<MediaCard
+					src="https://images.unsplash.com/photo-1724505599369-2c1d43324fdc"
+					title="Channeling their inner artist with a paintbrush and a splash of color!"
+					id={1234}
+					date={new Date("Oct 2, 2024")}
+				/>
+				<MediaCard
+					src="https://images.unsplash.com/photo-1724579243894-6a8c9bbfe88c"
+					title="Channeling their inner artist with a paintbrush and a splash of color!"
+					id={1234}
+					date={new Date("Oct 2, 2024")}
+				/>
+				<MediaCard
+					src="https://images.unsplash.com/photo-1724505599369-2c1d43324fdc"
+					title="Channeling their inner artist with a paintbrush and a splash of color!"
+					id={1234}
+					date={new Date("Oct 2, 2024")}
+				/>
+				<MediaCard
+					src="https://images.unsplash.com/photo-1723984834599-5357b87f727c"
 					title="Channeling their inner artist with a paintbrush and a splash of color!"
 					id={1234}
 					date={new Date("Oct 2, 2024")}
