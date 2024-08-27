1364994bc720410f02a69742dd69ffe4997dcaa3
Duc Ho <hotienduc05012002@gmail.com>
2024-08-26 22:46:02 -0600

    Moved mock data to props

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -1,27 +1,37 @@
 import { Copy } from "@phosphor-icons/react";
 
-function MediaCard() {
+interface MediaCardProps {
+	src: string;
+	title: string;
+	date: Date;
+	id: number;
+}
+
+const options: Intl.DateTimeFormatOptions = {
+	year: "numeric",
+	month: "short",
+	day: "numeric",
+};
+
+function MediaCard({ src, title, date, id }: MediaCardProps) {
 	return (
 		<div className=" flex flex-col rounded-lg">
 			<div className="relative rounded-lg">
 				<img
-					src="https://images.unsplash.com/photo-1724579243894-6a8c9bbfe88c"
+					src={src}
 					alt="Image"
 					className="object-fit rounded-t-lg"
 				/>
 			</div>
 			<div className="bg-white p-2 rounded-b-lg flex flex-col gap-4">
-				<h3 className="text-body-reg">
-					Channeling their inner artist with a paintbrush and a splash
-					of color!
-				</h3>
+				<h3 className="text-body-reg">{title}</h3>
 				<div className="flex justify-between">
 					<span className="text-sm text-neutrals-dark-200">
-						Oct.2, 2023
+						{date.toLocaleDateString("en-US", options)}
 					</span>
 					<div className="flex items-center gap-2">
 						<span className="text-sm text-neutrals-dark-300">
-							Id: 1234
+							Id: {id}
 						</span>
 						<button>
 							<Copy></Copy>
--- src/routes/test/MediaCardTestRoute.tsx
+++ src/routes/test/MediaCardTestRoute.tsx
@@ -4,7 +4,12 @@ function MediaCardTestRoute() {
 	return (
 		<div className="bg-blue-200 h-screen flex justify-center items-center">
 			<div className="w-[360px]">
-				<MediaCard />
+				<MediaCard
+					src="https://images.unsplash.com/photo-1724579243894-6a8c9bbfe88c"
+					title="Channeling their inner artist with a paintbrush and a splash of color!"
+					id={1234}
+					date={new Date("Oct 2, 2024")}
+				/>
 			</div>
 		</div>
 	);
