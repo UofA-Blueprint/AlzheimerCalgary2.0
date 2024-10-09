16bf1c87d614fc916fc91ff9bf83e30cc574e383
Khanh Bui <duykhanh.eric@gmail.com>
2024-10-09 01:37:57 -0600

    added the backend

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,29 +1,110 @@
-//#region Imports
+import React, { useState, useEffect } from "react";
 import { CaretLeft } from "@phosphor-icons/react";
 import MediaUploadZone from "./MediaUploadZone";
-import picture from "@/assets/images/pic1.jpg";
-//#endregion
+import { getStorage, ref, uploadBytesResumable, listAll, getDownloadURL } from "firebase/storage";
+import Button from "@/components/Button";
+import MediaUploadStatus from "./MediaUploadStatus";
+
+interface FileUploadStatus {
+	file: File;
+	progress: number;
+}
 
 function Gallery() {
-	//TODO: Configure the local gallery to display images here
-	const imgList: string[] = [picture, picture, picture, picture, picture];
+	const [uploads, setUploads] = useState<FileUploadStatus[]>([]);
+	const [imgList, setImgList] = useState<string[]>([]);
 
-	//#region Functions
+	// Create a root reference
+	const storage = getStorage();
+
+	/**
+	 * Extract current pictures in /images inside firebase storage
+	 */
+	const displayImages = async () => {
+		const listRef = ref(storage, "/images"); 
+		try {
+			const res = await listAll(listRef);
+			const imageUrls = await Promise.all(
+				res.items.map((itemRef) => getDownloadURL(itemRef))
+			);
+			setImgList(imageUrls);
+		} catch (error) {
+			console.error("Error displaying images: ", error);
+		}
+	};
 
 	/**
 	 * Close the gallery
 	 */
 	const handleClose = () => {};
 
+	// Useeffect to call the function to fetch images on component mount
+	useEffect(() => {
+		displayImages(); // 
+	}, []);
+
 	/**
 	 * Handle an image in the gallery being clicked
 	 */
 	const handleImageClicked = () => {};
 
-	//#endregion
+	// Function to handle adding files and simulate 1-second upload process
+	const handleFilesAdded = (files: File[]) => {
+		files.forEach((file) => {
+			// Create a new upload status object
+			const newUpload: FileUploadStatus = {
+				file,
+				progress: 0, 
+			};
+
+			// Add the new upload to the uploads state
+			setUploads((prevUploads) => [...prevUploads, newUpload]);
+
+			// Create a reference to where the file will be stored
+			const fileRef = ref(storage, `images/${file.name}`);
+
+			// Create a file upload task
+			const uploadTask = uploadBytesResumable(fileRef, file);
+
+			// Monitor the file upload progress
+			uploadTask.on(
+				"state_changed",
+				(snapshot) => {
+					const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					console.log(`Upload is ${progress}% done`);
+
+					// Update the progress state for the UI
+					setUploads((prevUploads) =>
+						prevUploads.map((prevUpload) =>
+							prevUpload.file.name === file.name
+								? { ...prevUpload, progress }
+								: prevUpload
+						)
+					);
+				},
+				(error) => {
+					// Handle errors
+					console.error("Upload failed", error);
+				},
+				async () => {
+					try {
+						const downloadURL = await getDownloadURL(fileRef);
+						console.log("File available at", downloadURL);
+
+						// Add the download URL to the image list
+						setImgList((prevImgList) => [...prevImgList, downloadURL]);
+						
+
+					} catch (error) {
+						console.error("Error getting download URL:", error);
+					}
+				}
+			);
+		});
+	};
 
 	return (
-		<div className="flex flex-col items-center justify-center gap-y-6 max-w-5xl px-6 rounded-xl py-8 bg-white">
+		<div className="flex flex-col items-center justify-center gap-y-6 max-w-5xl px-6 rounded-xl py-8 bg-white w-5/6">
 			{/* Title */}
 			<div className="w-full">
 				<div className="flex gap-x-2 items-center font-display text-2xl font-bold">
@@ -37,16 +118,20 @@ function Gallery() {
 			</div>
 
 			{/* Image gallery */}
-			<div className=" grid h-96 w-full grid-cols-2 md:grid-cols-4 gap-6 items-center overflow-y-auto scroller">
-				{imgList.map((img, index) => (
-					<img
-						key={index}
-						src={img}
-						alt="gallery"
-						className="w-56 aspect-square rounded-xl cursor-pointer hover:scale-95 transition ease-in-out duration-200"
-						onClick={handleImageClicked}
-					/>
-				))}
+			<div className={`grid h-96 w-full grid-cols-2 md:grid-cols-4 gap-6 items-center overflow-y-auto scroller min-h-[24rem]`}>
+				{imgList.length > 0 ? (
+					imgList.map((img, index) => (
+						<img
+							key={index}
+							src={img}
+							alt="gallery"
+							className="w-56 aspect-square rounded-xl cursor-pointer hover:scale-95 transition ease-in-out duration-200"
+							onClick={handleImageClicked}
+						/>
+					))
+				) : (
+					<p className="col-span-full text-center text-gray-500">No images available</p>
+				)}
 			</div>
 
 			{/* Or */}
@@ -58,7 +143,37 @@ function Gallery() {
 
 			{/* Upload media */}
 			<h2 className="w-full text-xl">Upload Media</h2>
-			<MediaUploadZone />
+			<MediaUploadZone onFilesAdded={handleFilesAdded} />
+
+			{/* Display upload status */}
+			<div className="w-full flex flex-col gap-4 mt-4">
+				{uploads.map((upload, index) => (
+					<MediaUploadStatus
+						key={index}
+						fileName={upload.file.name}
+						fileSize={upload.file.size}
+						uploadProgress={upload.progress}
+						setUploadProgress={() => {}}
+					/>
+				))}
+			</div>
+
+			{/* Buttons */}
+			<div className="flex flex-row gap-5 w-full">
+				<Button
+					onClick={() => {}}
+					text={"Cancel"}
+					rounded={false}
+					fill={false}
+					className="text-red-600 border-red-600 active:bg-red-600 active:text-red-600 before:bg-red-600 w-2/5"
+				/>
+				<Button
+					onClick={() => {}}
+					disabled={false}
+					text="Add Picture"
+					className="w-3/5"
+				/>
+			</div>
 		</div>
 	);
 }
--- src/components/MediaUploadZone.tsx
+++ src/components/MediaUploadZone.tsx
@@ -1,153 +1,80 @@
+import React, { useState, useEffect } from "react";
 import { UploadSimple, WarningCircle } from "@phosphor-icons/react";
-import { useState, useEffect, useRef } from "react";
-import { twMerge } from "tailwind-merge";
 import MediaUploadStatus from "./MediaUploadStatus";
 
-//#region interface
 interface MediaUploadZoneProps {
-	className?: string;
+  onFilesAdded: (files: File[]) => void;
+  className?: string;
 }
-//#endregion
 
-const MediaUploadZone = ({ className }: MediaUploadZoneProps) => {
-	const [isDragOver, setIsDragOver] = useState(false);
-	const [droppedFiles, setDroppedFiles] = useState<File[]>([]);
-	const [isValidFile, setIsValidFile] = useState(true);
-	const [uploadProgress, setUploadProgress] = useState(0);
+const MediaUploadZone = ({ onFilesAdded, className }: MediaUploadZoneProps) => {
+  const [isDragOver, setIsDragOver] = useState(false);
+  const [isValidFile, setIsValidFile] = useState(true);
 
-	const UploadZoneClassName =
-		"w-full h-full border-2 border-dashed flex flex-col justify-center items-center rounded-md border-primary-main text-base" +
-		(isDragOver
-			? " transform transition-transform duration-300 border-primary-dark ease-in-out scale-95"
-			: "");
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const files = Array.from(event.dataTransfer.files);
+    const validFiles = filterValidFiles(files);
+    onFilesAdded(validFiles);
+    setIsDragOver(false);
+  };
 
-	// handle drop events, drop the PNG, JPEG and GIF type of file
-	const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
-		event.preventDefault();
-		const files = Array.from(event.dataTransfer.files);
-		const validFiles = filterValidFiles(files);
+  const filterValidFiles = (files: File[]): File[] => {
+    const allowedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+    return files.filter((file) => {
+      const extension = file.name.toLowerCase().slice(file.name.lastIndexOf("."));
+      const isValid = allowedExtensions.includes(extension);
+      setIsValidFile(isValid);
+      return isValid;
+    });
+  };
 
-		setDroppedFiles((prevFiles) => [...prevFiles, ...validFiles]);
-		setIsDragOver(false);
-	};
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragOver(true);
+  };
 
-	const filterValidFiles = (files: File[]): File[] => {
-		const allowedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+  const handleDragLeave = () => {
+    setIsDragOver(false);
+  };
 
-		return files.filter((file) => {
-			const extension = file.name
-				.toLowerCase()
-				.slice(file.name.lastIndexOf("."));
-			const isValid = allowedExtensions.includes(extension);
-			isValid ? setIsValidFile(true) : setIsValidFile(false);
-			console.log(
-				`File ${file.name} is ${isValid ? "valid" : "invalid"}.`,
-			);
-			return isValid;
-		});
-	};
-
-	const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-		event.preventDefault();
-		setIsDragOver(true);
-	};
-
-	const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
-		event.preventDefault();
-		setIsDragOver(false);
-	};
-
-	useEffect(() => {
-		console.log("Dropped files:", droppedFiles);
-	}, [droppedFiles]);
-
-	return (
-		<div
-			className={twMerge(
-				"w-full h-full font-display flex flex-col",
-				className,
-			)}
-		>
-			<div
-				className={UploadZoneClassName}
-				onDrop={handleDrop}
-				onDragOver={handleDragOver}
-				onDragLeave={handleDragLeave}
-			>
-				<div className="flex flex-col justify-center items-center gap-y-2 p-8">
-					<UploadSimple className="text-3xl opacity-80" />
-					<p className="text-lg opacity-80">Drag and Drop here</p>
-					<p className="text-lg opacity-80">or</p>
-
-					<input
-						type="file"
-						id="fileInput"
-						onChange={(event) => {
-							if (event.target.files) {
-								const files = Array.from(event.target.files);
-								const validFiles = filterValidFiles(files);
-								setDroppedFiles((prevFiles) => [
-									...prevFiles,
-									...validFiles,
-								]);
-							}
-						}}
-						className="hidden"
-					/>
-					<label
-						htmlFor="fileInput"
-						className="text-primary-main cursor-pointer text-lg font-bold hover:text-primary-light transition ease-in-out"
-					>
-						Browse file
-					</label>
-				</div>
-				{droppedFiles.length > 0 && (
-					<div className="overflow-hidden">
-						{/* {droppedFiles.slice(0, 3).map((file, index) => (
-							<div
-								key={index}
-								className="truncate w-full text-center opacity-80"
-							>
-								{file.name}
-							</div>
-						))} */}
-						{droppedFiles.length > 3 && <div>...</div>}
-					</div>
-				)}
-			</div>
-			<div className="flex flex-row justify-between text-sm text-gray-500 my-2 font-display">
-				<div>Accepted file types: png, jpeg</div>
-				<div>Max size: 25MB</div>
-			</div>
-			{!isValidFile && (
-				<div className="flex flex-row text-sm items-center gap-x-2 text-red-500 font-display">
-					<WarningCircle></WarningCircle> File type not supported
-				</div>
-			)}
-
-			{/* Upload Status */}
-			<div className="mt-4 flex flex-col gap-y-4">
-				<MediaUploadStatus
-					fileName="Testing.png"
-					fileSize={1000000000}
-					uploadProgress={uploadProgress}
-					setUploadProgress={setUploadProgress}
-				/>
-				<MediaUploadStatus
-					fileName="Testing.png"
-					fileSize={1000000000}
-					uploadProgress={50}
-					setUploadProgress={setUploadProgress}
-				/>
-				<MediaUploadStatus
-					fileName="Testing2.png"
-					fileSize={1000000000}
-					uploadProgress={100}
-					setUploadProgress={setUploadProgress}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={`w-full h-full font-display flex flex-col ${className}`}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+    >
+      <div className="w-full h-full border-2 border-dashed flex flex-col justify-center items-center rounded-md border-primary-main text-base">
+        <UploadSimple className="text-3xl opacity-80" />
+        <p className="text-lg opacity-80">Drag and Drop here</p>
+        <p className="text-lg opacity-80">or</p>
+        <input
+          type="file"
+          id="fileInput"
+          multiple
+          onChange={(event) => {
+            const files = Array.from(event.target.files || []);
+            const validFiles = filterValidFiles(files);
+            onFilesAdded(validFiles);
+          }}
+          className="hidden"
+        />
+        <label
+          htmlFor="fileInput"
+          className="text-primary-main cursor-pointer text-lg font-bold hover:text-primary-light transition ease-in-out"
+        >
+          Browse file
+        </label>
+      </div>
+      {!isValidFile && (
+        <div className="flex text-red-500 items-center">
+          <WarningCircle />
+          <span>File type not supported</span>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MediaUploadZone;
